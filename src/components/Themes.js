0f36ded49b57f895396e7437c6af16d40c350d3a
face <contactoface.22@gmail.com>
2022-07-08 21:50:34 -0300

    se detecto un error

--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,26 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../CSS/themes.css'
 import sun from '../multimedia/sun.svg'
 import moon from '../multimedia/moon.svg'
 
 
 export default function Themes() {
+    const [tema, setTema]= useState(true)
     const btnSwitch= document.querySelector("#switch")
     const cambiar=()=>{
         document.body.classList.toggle("dark")
         btnSwitch.classList.toggle("active")
-
+        setTema(!tema)
     }
-
+    console.log("tema",tema)
   return (
     <div className='themeContainer'>
         <button className='switch' id='switch'onClick={cambiar}>
-            <span>
-                <img id='iconSun' src={sun} alt="icono de sol"></img>
-            </span>
-            <span>
-                <img id='iconSun' src={moon} alt="icono de luna"></img>
-            </span>
+            {tema?
+                <span>
+                    <img id='iconSun' src={moon} alt="icono de luna"></img>
+                </span>
+                :
+                <span>
+                    <img id='iconSun' src={sun} alt="icono de sol"></img>
+                </span>
+            }
+
+
         </button>
     </div>
   )
