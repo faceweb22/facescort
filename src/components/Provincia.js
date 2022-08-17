89cf447a66850ce3a1d7658949e049793df9e86a
jcsamgit <jcv.gerald@gmail.com>
2022-08-17 15:19:35 -0300

    SE ELIMINO LA VERIFICACION EDAD

--- src/components/Provincia.js
+++ src/components/Provincia.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../multimedia/logo.png'
-import Terminos from './Terminos'
+// import Terminos from './Terminos'
 
 export default function Provincia() {
-    const verificacion=()=>{
-        // let body= document.querySelector("body")
-        let verificadorEdad=document.querySelector("#terminosContainer")  
-        verificadorEdad.style.display="block"
-    }
+    // const verificacion=()=>{
+    //     // let body= document.querySelector("body")
+    //     let verificadorEdad=document.querySelector("#terminosContainer")  
+    //     verificadorEdad.style.display="block"
+    // }
     
   return (
     <div className='main' id='main'>
@@ -26,17 +26,18 @@ export default function Provincia() {
         <h2 id='seleccion'>Seleccione una provincia</h2>
         <div className='provContainer'>
             
-            {/* <Link to="/buenos-aires"> */}
-                <div className='provincias' onClick={verificacion}>
+            <Link to="/buenos-aires">
+                <div className='provincias'>
+                {/* <div className='provincias' onClick={verificacion}> */}
                     Buenos Aires
                 </div>
-            {/* </Link> */}
+            </Link>
             <Link to="#">
                 <div className='provincias'>
                     Proximamente mas provincias...
                 </div>
             </Link>
-            <Terminos/>
+            {/* <Terminos/> */}
         </div>
         
     
