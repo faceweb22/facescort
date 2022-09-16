75ff7f9df58b73a5a51285121f81949df8764076
faceweb22 <106940401+faceweb22@users.noreply.github.com>
2022-09-16 01:05:48 -0300

    se elimino a zoe

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,60 +1,6 @@
 
 export const data= [ 
-    {   
-    id:"1",
-    nombre:"Zoe",
-    wp: "1166049454",
-    ubicacion: "microcentro",
-    portada:"https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fportada.jpeg?alt=media&token=c6cdde21-0956-49fa-aabe-57b3b832426c",
-    fotos: [
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe.jpeg?alt=media&token=d7ed41a9-eacc-4194-9450-5aeb2fc68fe7",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(1).jpeg?alt=media&token=8ecd1c91-b832-4516-86b6-5101699adb72",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(2).jpeg?alt=media&token=efb0be03-4411-4ea2-802d-29ef18d7f5cf",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(3).jpeg?alt=media&token=a61bc535-57ae-4934-b2d9-fe8cfdfeb300",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(4).jpeg?alt=media&token=3994ecba-0ecc-4426-a5ad-e130abfb2e4f",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(5).jpeg?alt=media&token=3f2d3ea4-0774-4ede-9ae2-dd7577be0745",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(6).jpeg?alt=media&token=d4f6f23e-8827-4933-8306-4250f417a1a1",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(7).jpeg?alt=media&token=5d259504-7707-4570-b122-33ad0c9d11b9",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(8).jpeg?alt=media&token=8bffde69-1ee0-4b0f-8b70-df7c5b49dca9",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(9).jpeg?alt=media&token=30e51068-3d4c-44ae-a09f-cb161ee10d2c",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(10).jpeg?alt=media&token=ec5190ea-0ee3-4bfe-aea9-875eca0b6a3f",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(11).jpeg?alt=media&token=2d64c91c-8327-4ec6-8edd-9dd16bfbe84f",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(12).jpeg?alt=media&token=ddac5f16-d7a2-4064-b493-5228fba83997",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(13).jpeg?alt=media&token=24b19cae-2204-4cf4-b054-46373fdba112",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(14).jpeg?alt=media&token=8a1d45bc-ce97-4f96-8cde-9cc8b75dc8b9",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(15).jpeg?alt=media&token=a1fc9d1b-5d83-4b75-9f91-7838ca0389c0",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(16).jpeg?alt=media&token=f31247de-3f5b-4847-a43f-7ae6eca1209c",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(17).jpeg?alt=media&token=44298a11-f924-4aa9-bf7c-db99ea0e1e8f",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(18).jpeg?alt=media&token=bb84f238-5676-4b73-8cb0-7325a1f00afd",  
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%20(19).jpeg?alt=media&token=e2e5fa28-9577-4f2d-bb71-9c97e8f9dc10"
-    ],
-    videos:[
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%201ra%20parte.mp4?alt=media&token=260c2fd3-7cab-4fde-9d63-1a8c60620b64,",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%202da%20parte.mp4?alt=media&token=28cc6f99-6175-4807-a4c7-03533d2ada56",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%203ra%20parte.mp4?alt=media&token=afe88060-cb66-4950-9253-d48817b41a98",
-      "https://firebasestorage.googleapis.com/v0/b/publicidades-multimedia.appspot.com/o/argentina%2Fbuenos%20aires%2Fzoe%2066049454%2Fzoe%204ta%20parte.mp4?alt=media&token=370246a0-c5c7-4e7f-a2dd-d94ccb1b589a"
-    ],
-    descripcion: ["¡Hola amor! Soy Zoe, tu putita virtual embarazada preferida ❤‍🔥",
-    <b>"IMPORTANTE ❗HOY ESTOY HACIENDO PREMIO DE BIENVENIDA PARA TODOS LOS QUE ME HAYAN VISTO EN FACE ESCORT! 💖"</b>,
-    
-    "Soy MUY MORBOSA, demisexual y los encuentros los hago GRATIS con mis clientes virtuales frecuentes ¿Querés conocerme? ",
-    
-    "😍 Tengo pack de FOTOS Y VIDEOS: de masturbación femenina, baile erótico con masturbación anal y vaginal, orgasmos reales femeninos, doble penetración con juguetes, eyaculación masculina en cara, acabada en boca, creampie, doble facial (mascara facial y semen), doble creampie sin sacarla, pete y sexo en la montaña, facial en la montaña, juego manual con juguetes para penes, sexo con mi hermano de sangre, videos embarazada, JOI embarazada (masturbación guiada), striptease embarazada, conejita preñada, food porn embarazada (con lemon pie), sexo anal embarazada, creampie anal,  eyaculación femenina embarazada (tipo cremita), juego de roll de bebita y gatita con diversos juguetes (cogida y masturbacion, bdsm soft con spank), sumisa facializada, scat, lluvia dorada (embarazada y sin estarlo), lluvia marrón, lluvia blanca (leche de las tetas), enfermera, dándole la teta a mi hermano, sexo anal, mi primer anal, juego con plugs, mast con el joystick de mi hermano, extrayendome la leche de las tetas. ",
-    "El contenido es en buena calidad: full HD y 4k.",
-    
-    "🥵 Hago sexting convencional, dominación, humillación, etc." ,
-    
-    "😍 Audios profesionales! Nunca antes escuchaste NADA IGUAL, te hacen volar la cabeza." ,
-    
-    "🍼 Videollamada Sacándome LECHE DE LAS TETAS y masturbándome hasta acabar. ",
-    
-    "😳 Videos personalizados cumpliendo todas tus fantasías." ,
-    
-    "🤯 Experta en dick rate y cum rate." ,
-    
-    "❤‍🔥 Relato erótico de INCESTO REAL de cómo y por qué empezamos a tener sexo con mi hermano."
-  ]
-  },
+ 
   {   
     id:"2",
     nombre:"Nombre",
